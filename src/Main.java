a181d6e40a73d352c71afb85e1b3061f39f19c2d
soknem <sokname56@gmail.com>
2025-01-31 20:49:40 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,50 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    static String[] seats=new String[50];
+    static String[] seats = new String[50];
+    static String[] histories = new String[50];
 
-   static String[] extractSeat(String seatsString){
+    static String[] extractSeat(String seatsString) {
 
-        return seatsString.replace(" ","").split(",");
+        return seatsString.replace(" ", "").split(",");
 
     }
 
-   static void books(String[] seatsString){
+    static void books(String[] seatsString) {
 
-       for (String s : seatsString) {
+        for (String s : seatsString) {
 
-           seats[Integer.parseInt(s) - 1] = "BC";
+            seats[Integer.parseInt(s) - 1] = "BC";
 
-       }
-    }
-
-    static String checkingSeats(String[] checkinSeats){
-
-       String bcSeats=null;
-
-       for (String s : checkinSeats) {
-           //seats[5]=BC
-           if(seats[Integer.parseInt(s) - 1]!=null){
-
-               if(bcSeats==null){
-                   bcSeats=s+",";
-               }else{
-                   bcSeats+=s+",";
-               }
-
-           }
         }
-       return bcSeats;
+    }
+
+    static Boolean validation(String text, String regex) {
+
+        return Pattern.compile(regex).matcher(text).matches();
+
+    }
+
+    static String checkingSeats(String[] checkinSeats) {
+
+        String bcSeats = null;
+
+        for (String s : checkinSeats) {
+            //seats[5]=BC
+            if (seats[Integer.parseInt(s) - 1] != null) {
+
+                if (bcSeats == null) {
+                    bcSeats = s + ",";
+                } else {
+                    bcSeats += s + ",";
+                }
+
+            }
+        }
+        return bcSeats;
     }
 
 
@@ -43,31 +52,36 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        seats[4]="BC";//5
-        seats[6]="BC";//7
+        seats[4] = "BC";//5
+        seats[6] = "BC";//7
 
-        do{
+        do {
 
-            System.out.print("Enter number of seats: ");
+            String seatsInput;
+            do{
 
-            String seatsInput = scanner.nextLine();
+                System.out.print("Enter number of seats: ");
+
+                seatsInput = scanner.nextLine();
+
+            }while (!validation(seatsInput, "([1-9])|([1-9][0-9,]+)"));
 
             String[] seatsEx = extractSeat(seatsInput);
 
-            String checkSeats =checkingSeats(seatsEx);
+            String checkSeats = checkingSeats(seatsEx);
 
-            if(checkSeats==null){
+            if (checkSeats == null) {
                 books(seatsEx);
                 break;
-            }else {
-                System.out.println("Seat = "+checkSeats+"\balready booked!!!!");
+            } else {
+                System.out.println("Seat = " + checkSeats + "\balready booked!!!!");
             }
 
-        }while (true);
+        } while (true);
 
 
-        for(String item:seats){
-            System.out.println("item:"+(item==null?"GREEN":"RED"));
+        for (String item : seats) {
+            System.out.println("item:" + (item == null ? "GREEN" : "RED"));
         }
 
     }
